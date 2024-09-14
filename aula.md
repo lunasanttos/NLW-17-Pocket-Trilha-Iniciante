a7d73313e6f44108cc43e7cccea9306e561369ee
luna santos <oluna8466@gmail.com>
2024-09-13 23:54:06 -0300

    Finalizando

--- aula.md
+++ aula.md
@@ -66,6 +66,13 @@
 
   - [x] Importação de módulos (require, CommonJS)
   - [x] Biblioteca 'inquirer' para criar prompts interativos
+  - [x] FS (file system)
+
+  ## JSON 
+
+  - [x] JavaScript Object Notation (.json)
+  - [x] JSON.parse(): transforma de JSON para JS
+  - [x] JSON.stringfy: transforma de JS para JSON
 
   # Programação assíncrona e Promises
 
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const {select, input, checkbox} = require('@inquirer/prompts')
-
+const fs = require("fs").promises
 
 let mensagem = "Bem-vindo ao App de Metas";
 
-let meta = {
-  value: 'Tomar 3L de água por dia',
-  checked: false
+let metas
+
+const carregarMetas = async () => {
+  try {
+      const dados = await fs.readFile("metas.json", "utf-8")
+      metas = JSON.parse(dados)
+  }
+  catch (erro) {
+      metas = []
+  }
 }
 
-let metas = [meta]
+const salvarMetas = async () => {
+  await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
 
 const cadastrarMeta = async () => {
   const meta = await input({message: "Digite a meta: "})
@@ -26,6 +35,10 @@ const cadastrarMeta = async () => {
 }
 
 const listarMetas = async () => {
+  if (metas.length == 0) {
+    mensagem = "Não existem metas!"
+    return
+  }
   const respostas = await checkbox ({
     message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o enter para finalizar essa etapa",
     choices: [...metas],
@@ -131,8 +144,11 @@ const mostrarMensagem = () => {
   }
 }
 const start  = async () => {
+  await carregarMetas()
+
     while(true){
       mostrarMensagem()
+      await salvarMetas()
 
       const opcao = await select({
         message: "Menu >",
